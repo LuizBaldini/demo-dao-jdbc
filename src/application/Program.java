a40e39577260da030f2442989c73f919766dad8a
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-02-18 14:11:21 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
         Department obj = new Department(1,"Books");
         System.out.println(obj);
         Seller seller = new Seller(21, "Bob" ,"bob@gmail.com", new Date(),3000,obj);
-        System.out.println(seller);""
+        System.out.println(seller);
 
     }
 }
