0bfaa07b5625b67b075935fd8819a80bc5bb226f
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-02-19 15:28:01 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,6 +29,9 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
-
+        System.out.println("=== TESTE 04 seller insert=== ");
+        Seller newSeller = new Seller(null,"Greg","greg@gmail.com", new Date(),1000,department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted ID: " + newSeller.getId());
     }
 }
