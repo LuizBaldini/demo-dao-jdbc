89a6fe02ff7503176e1165684a3cd2a3339c675d
LuizBaldini <luizfelipebaldini.07@gmail.com>
2026-02-20 10:09:09 -0300

    DepartmentDao implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -144,7 +144,6 @@ public class SellerDaoJDBC implements SellerDao {
                             "WHERE seller.Id = ?");
 
             st.setInt(1, id);
-
             rs = st.executeQuery();
             if (rs.next()) {
                 Department dep = instantiateDepartment(rs);
